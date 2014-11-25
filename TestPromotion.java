0000be233a1aa785b7d88793302f765536824761
19920626 <xiefeng_626@163.com>
2014-11-26 02:03:41 +0800

    Update TestPromotion.java

--- TestPromotion.java
+++ TestPromotion.java
@@ -11,8 +11,8 @@ public class TestPromotion {
 		 //有延迟地重复多次，在提交前测试时都出现了两次购买成功的案例。
 		 //因此个人认为已经完成并发多线程的实验。
 		for(int i=0;i<2000;i++){
-			Thread t = new BuyPlan(phone,price);
-		    t.start();
+			Thread doBuyPlan = new BuyPlan(phone,price);
+			doBuyPlan.start();
 		}
 		/*for(int i=0;i<8;i++){//随机产生号码
 			  //phone="";
@@ -20,24 +20,24 @@ public class TestPromotion {
 			  for (int j = 0; j < 5; j++) {
 				phone=phone+(int)(Math.random()*10);
 			}
-		      Thread t = new Buy(phone,price);
-		      t.start();
+		      Thread doBuyPlan = new Buy(phone,price);
+		      doBuyPlan.start();
 		}
 		
 		phone ="12345";
-		Thread t = new Buy(phone,price);
-	    t.start();
+		Thread doBuyPlan = new Buy(phone,price);
+	    doBuyPlan.start();
 	    //测试重复参加的用户
 	    phone="12345";
-	    t = new Buy(phone,price);
-	    t.start();
+	    doBuyPlan = new Buy(phone,price);
+	    doBuyPlan.start();
 	    phone="23456";
-	    t = new Buy(phone,price);
-	    t.start();
+	    doBuyPlan = new Buy(phone,price);
+	    doBuyPlan.start();
 	    //测试已经结束了的活动
 	    phone="2345632";
-	    t = new Buy(phone,price);
-	    t.start();*/
+	    doBuyPlan = new Buy(phone,price);
+	    doBuyPlan.start();*/
 	}
 
 }
