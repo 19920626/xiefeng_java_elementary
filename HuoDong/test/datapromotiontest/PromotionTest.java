ec4ea21e74cd65e34ef2f29a119d0d2b34da8b3e
19920626 <xiefeng_626@163.com>
2014-11-30 19:59:19 +0800

    add project HuoDong

--- HuoDong/test/datapromotiontest/PromotionTest.java
+++ HuoDong/test/datapromotiontest/PromotionTest.java
@@ -7,27 +7,27 @@ import datapromotion.Promotion;
 
 public class PromotionTest {
 
-	@Test
-	public void testDoPurchase() {
+    @Test
+    public void testDoPurchase() {
 		
-		for(int i=0;i<10;i++){
-			new Thread(){
-				String phone="12345";
-				int price=1;
-				public void run(){
-					int state = Promotion.doPurchase(phone, price);
-				    Handle doHandle = new Handle();
+        for(int i=0;i<10;i++){
+            new Thread(){
+                String phone="12345";
+                int price=1;
+                public void run(){
+                    int state = Promotion.doPurchase(phone, price);
+                    Handle doHandle = new Handle();
 				   
-				    doHandle.dealWith(state, phone);
+                    doHandle.dealWith(state, phone);
 				   
-				    try {
-					    sleep((int) Math.random() * 10); 	
-				    } catch (Exception e) { 
-					 	e.printStackTrace();
-				    }
-				}
-			}.start();
-		}	
-	}
+                    try {
+                        sleep((int) Math.random() * 10); 	
+                    } catch (Exception e) { 
+                        e.printStackTrace();
+                    }
+                }
+            }.start();
+        }	
+    }
 
 }
