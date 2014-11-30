38dac51756877dbe044fb89c466d1692a3f1ee5b
19920626 <xiefeng_626@163.com>
2014-11-30 22:41:52 +0800

    Revert "Revert "add project HuoDong""
    This reverts commit 17c4897789ac6cc5735e6e1424403a8c89b4dcfa.
    
    Conflicts:
    	HuoDong/test/datapromotiontest/PromotionTest.java

--- HuoDong/test/datapromotiontest/PromotionTest.java
+++ HuoDong/test/datapromotiontest/PromotionTest.java
@@ -61,9 +61,31 @@ public class PromotionTest {
 
     @Test
     public void testDoPurchase() {
+<<<<<<< HEAD
         int state = Promotion.doPurchase(phone, price);
         
         assertEquals(result, state);
+=======
+		
+        for(int i=0;i<10;i++){
+            new Thread(){
+                String phone="12345";
+                int price=1;
+                public void run(){
+                    int state = Promotion.doPurchase(phone, price);
+                    Handle doHandle = new Handle();
+				   
+                    doHandle.dealWith(state, phone);
+				   
+                    try {
+                        sleep((int) Math.random() * 10); 	
+                    } catch (Exception e) { 
+                        e.printStackTrace();
+                    }
+                }
+            }.start();
+        }	
+>>>>>>> parent of 17c4897... Revert "add project HuoDong"
     }
 
 }
