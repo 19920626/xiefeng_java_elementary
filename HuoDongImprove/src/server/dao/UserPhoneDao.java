2ec1937a4e5f40727c7fa2b8d9af140458aeab03
19920626 <xiefeng_626@163.com>
2014-12-03 00:41:16 +0800

    Revert "add project HuoDongImprove"
    This reverts commit 76edcdbcd50f95993825be6fd33cf4f6333c9750.

--- HuoDongImprove/src/server/dao/UserPhoneDao.java
+++
@@ -1,9 +0,0 @@
-package server.dao;
-
-import server.entity.UserPhone;
-
-public interface UserPhoneDao {
-    public long addPhone(UserPhone phone); //插入一个用户的号码
-    public int findUser(UserPhone phone);  //找到相应的用户
-    public int countAll(); //统计所有参与用户
-}
