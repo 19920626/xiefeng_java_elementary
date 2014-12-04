c510eceef011b8f1a394565236ee1cb8e5e0817e
19920626 <xiefeng_626@163.com>
2014-12-04 20:57:44 +0800

    create UserPhoneImpl.java

---
+++ HuoDongImprove/src/server/daoimpl/UserPhoneImpl.java
@@ -0,0 +1,117 @@
+package server.daoimpl;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import server.dao.UserPhoneDao;
+import server.db.DataConnection;
+import server.entity.UserPhone;
+
+public class UserPhoneImpl implements UserPhoneDao {
+
+    @Override
+    public long addPhone(UserPhone phone) {
+        int flag = 0; //是否成功
+        DataConnection dConn = new DataConnection();
+        Connection conn = dConn.getConn();
+        PreparedStatement pStat = null;
+        
+        try { 
+            conn.setAutoCommit(false); //用事务处理的方式实现数据库安全
+            String sql = "insert into promotion (phone) values (?) ";
+            pStat = conn.prepareStatement(sql);
+            pStat.setLong(1, phone.getPhone()); 
+            flag = pStat.executeUpdate();
+            conn.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                conn.rollback();
+            } catch (SQLException e1) {
+                e1.printStackTrace();
+            }
+        } finally{
+            try {
+                pStat.close();
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return flag;
+    }
+
+    @Override
+    public int findUser(UserPhone phone) {
+        int flag = 0; //所查对象的下标，没有则为-1
+        DataConnection dConn = new DataConnection();
+        Connection conn = dConn.getConn();
+        PreparedStatement pStat = null;
+        
+        try {
+            conn.setAutoCommit(false);
+            String sql = "select id from promotion where phone = " + phone.getPhone() + "";
+            pStat = conn.prepareStatement(sql);
+            ResultSet rs = pStat.executeQuery();
+            
+            while(rs.next()) {
+                flag = rs.getInt(1);
+            }
+            conn.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                conn.rollback();
+            } catch (SQLException e1) {
+                e1.printStackTrace();
+            }
+        } finally{
+            try {
+                pStat.close();
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return flag;
+    }
+
+    @Override
+    public int countAll() {
+        int flag = 0; //所有参与的用户的总量
+        DataConnection dConn = new DataConnection();
+        Connection conn = dConn.getConn();
+        PreparedStatement pStat = null;
+        
+        try { 
+            conn.setAutoCommit(false);
+            String sql = "select count(*) from promotion";
+            pStat = conn.prepareStatement(sql);
+            ResultSet rs = pStat.executeQuery();
+            
+            while(rs.next()) {
+                flag = rs.getInt(1);
+            }
+                
+            conn.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                conn.rollback();
+            } catch (SQLException e1) {
+                e1.printStackTrace();
+            }
+        } finally{
+            try {
+                pStat.close();
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return flag;
+    }
+
+}
